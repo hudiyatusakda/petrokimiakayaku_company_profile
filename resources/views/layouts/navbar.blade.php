e80c63a0f2b2179f8815c5c9d861f0d10e638d5e
AryoXGG <=>
2025-12-14 22:58:41 +0700

    stash

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -3,39 +3,13 @@
     <li><a href="{{ route('about') }}">Tentang Perusahaan</a></li>
     <li><a href="{{ route('products') }}">Produk</a></li>
     <li><a href="{{ route('services') }}">Media</a></li>
-    {{-- <li><a href="{{ route('testimonials') }}">Testimonials</a></li>
-    <li><a href="{{ route('blog') }}">Blog</a></li>--}
 
-    {{-- <li class="dropdown"><a href="#"><span>Dropdown</span> <i class="bi bi-chevron-down toggle-dropdown"></i></a>
-      <ul>
-        <li><a href="#">Dropdown 1</a></li>
-        <li class="dropdown"><a href="#"><span>Deep Dropdown</span> <i
-              class="bi bi-chevron-down toggle-dropdown"></i></a>
-          <ul>
-            <li><a href="#">Deep Dropdown 1</a></li>
-            <li><a href="#">Deep Dropdown 2</a></li>
-            <li><a href="#">Deep Dropdown 3</a></li>
-            <li><a href="#">Deep Dropdown 4</a></li>
-            <li><a href="#">Deep Dropdown 5</a></li>
-          </ul>
-        </li>
-        <li><a href="#">Dropdown 2</a></li>
-        <li><a href="#">Dropdown 3</a></li>
-        <li><a href="#">Dropdown 4</a></li>
-      </ul>
-    </li> --}}
-
-    <!-- <li><a href="{{ route('contact') }}">icon cari</a></li>
-
-    <!-- <li class="nav-search"><a href="{{ route('contact') }}" class="nav-search-link">
-      <img src="{{ asset('assets/img/search.png') }}" alt="Cari">
-    </a></li> -->
     <li class="nav-search">
       <a href="{{ route('search') }}" class="nav-search-link" aria-label="Buka pencarian">
         <i class="bi bi-search" style="font-size:20px"></i>
       </a>
     </li>
-
   </ul>
+
   <i class="mobile-nav-toggle d-xl-none bi bi-list"></i>
 </nav>
