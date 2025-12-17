5a6feee92d2a701ba2d20928a2e66f5494f8ce2b
AryoXGG <=>
2025-12-17 11:12:18 +0700

    update footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,99 +1,79 @@
 <!-- footer -->
-
 <footer id="footer" class="footer dark-background">
-
   <div class="footer-top">
     <div class="container">
       <div class="row gy-4">
-        <div class="col-lg-4 col-md-6 footer-about">
+
+        <!-- LOGO -->
+        <div class="col-lg-3 col-md-6 footer-about">
           <a href="index.html" class="logo d-flex align-items-center">
-            <span class="sitename">AgriCulture</span>
+            <img src="{{ asset('assets/img/logo_bawah.png') }}"
+                 alt="Petrokimia Kayaku Logo"
+                 class="img-fluid footer-logo">
           </a>
-          <div class="footer-contact pt-3">
-            <p>A108 Adam Street</p>
-            <p>New York, NY 535022</p>
-            <p class="mt-3"><strong>Phone:</strong> <span>+1 5589 55488 55</span></p>
-            <p><strong>Email:</strong> <span>info@example.com</span></p>
-          </div>
         </div>
 
-        <div class="col-lg-2 col-md-3 footer-links">
-          <h4>Useful Links</h4>
-          <ul>
-            <li><a href="#">Home</a></li>
-            <li><a href="#">About us</a></li>
-            <li><a href="#">Services</a></li>
-            <li><a href="#">Terms of service</a></li>
-            <li><a href="#">Privacy policy</a></li>
-          </ul>
+        <!-- KANTOR PUSAT -->
+        <div class="col-lg-3 col-md-6 footer-contact">
+          <h4>KANTOR PUSAT:</h4>
+          <p>
+            Jl. Jendral Ahmad Yani<br>
+            PO Box 107 Gresik, 61119<br>
+            Jawa Timur - Indonesia<br>
+            Telp. (+6231) 3981815, 3981831, 3981989<br>
+            Fax (+6231) 3981830
+          </p>
         </div>
 
-        <div class="col-lg-2 col-md-3 footer-links">
-          <h4>Our Services</h4>
-          <ul>
-            <li><a href="#">Web Design</a></li>
-            <li><a href="#">Web Development</a></li>
-            <li><a href="#">Product Management</a></li>
-            <li><a href="#">Marketing</a></li>
-            <li><a href="#">Graphic Design</a></li>
-          </ul>
+        <!-- KANTOR CABANG -->
+        <div class="col-lg-3 col-md-6 footer-contact">
+          <h4>KANTOR CABANG:</h4>
+          <p>
+            Jl. Cisanggiri I/16 Blok Q-IV<br>
+            Kebayoran Baru<br>
+            Jakarta Selatan 12170<br>
+            Telp. (+6221) 7205453<br>
+            Fax (+6221) 7251244
+          </p>
         </div>
 
-        <div class="col-lg-2 col-md-3 footer-links">
-          <h4>Hic solutasetp</h4>
-          <ul>
-            <li><a href="#">Molestiae accusamus iure</a></li>
-            <li><a href="#">Excepturi dignissimos</a></li>
-            <li><a href="#">Suscipit distinctio</a></li>
-            <li><a href="#">Dilecta</a></li>
-            <li><a href="#">Sit quas consectetur</a></li>
-          </ul>
-        </div>
+        <!-- SOSMED -->
+        <div class="col-lg-3 col-md-6 footer-social">
+          <h4>FACEBOOK:</h4>
+          <p>Petani Kayaku</p>
 
-        <div class="col-lg-2 col-md-3 footer-links">
-          <h4>Nobis illum</h4>
-          <ul>
-            <li><a href="#">Ipsam</a></li>
-            <li><a href="#">Laudantium dolorum</a></li>
-            <li><a href="#">Dinera</a></li>
-            <li><a href="#">Trodelas</a></li>
-            <li><a href="#">Flexo</a></li>
-          </ul>
+          <h4>INSTAGRAM:</h4>
+          <p>@infopetrokayaku</p>
+
+          <h4>YOUTUBE:</h4>
+          <p>infomedia_kayaku</p>
         </div>
 
       </div>
     </div>
   </div>
 
-  <div class="copyright text-center">
-    <div
-      class="container d-flex flex-column flex-lg-row justify-content-center justify-content-lg-between align-items-center">
+  <!-- Language Switcher -->
+  <div class="footer-language-bar">
+    <div class="container">
+      <div class="language-wrapper">
+        <span class="label">LANGUAGE:</span>
 
-      <div class="d-flex flex-column align-items-center align-items-lg-start">
-        <div>
-          © Copyright <strong><span>MyWebsite</span></strong>. All Rights Reserved
-        </div>
-        <div class="credits">
-          <!-- All the links in the footer should remain intact. -->
-          <!-- You can delete the links only if you purchased the pro version. -->
-          <!-- Licensing information: https://bootstrapmade.com/license/ -->
-          <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/herobiz-bootstrap-business-template/ -->
-          Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
-        </div>
+        <a href="?lang=id" class="lang-item active">
+          <img src="{{ asset('assets/img/flags/indonesia.png') }}" alt="Indonesia">
+          <span>ID</span>
+        </a>
+
+        <a href="?lang=en" class="lang-item">
+          <img src="{{ asset('assets/img/flags/english.png') }}" alt="English">
+          <span>EN</span>
+        </a>
       </div>
-
-      <div class="social-links order-first order-lg-last mb-3 mb-lg-0">
-        <a href=""><i class="bi bi-twitter-x"></i></a>
-        <a href=""><i class="bi bi-facebook"></i></a>
-        <a href=""><i class="bi bi-instagram"></i></a>
-        <a href=""><i class="bi bi-linkedin"></i></a>
-      </div>
-
     </div>
   </div>
-
 </footer>
 <!-- footer -->
+
 
 <!-- Scroll Top -->
 <a href="#" id="scroll-top" class="scroll-top d-flex align-items-center justify-content-center"><i
