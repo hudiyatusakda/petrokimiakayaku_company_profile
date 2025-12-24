5c0ce9e1b488596ca55b73a701e35f1151fbfcb1
AryoXGG <=>
2025-12-24 11:01:47 +0700

    update produk

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,5 +1,6 @@
 <nav id="navmenu" class="navmenu">
   <ul>
+    <li><a href="{{ route('home') }}">Beranda</a></li>
     <li><a href="{{ route('about') }}">Profil Perusahaan</a></li>
     <li><a href="{{ route('products') }}">Produk</a></li>
     <li><a href="{{ route('services') }}">Media</a></li>
