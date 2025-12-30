0f9f5c913de7362194d36bcae76516c654032ed6
AryoXGG <=>
2025-12-30 16:17:00 +0700

    update dropdown

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -2,24 +2,32 @@
   <ul>
 
     <!-- BERANDA + MEGA DROPDOWN -->
-    <li class="mega-dropdown">
-      <a href="{{ route('home') }}">Beranda</a>
+   <li class="mega-dropdown">
+  <a href="{{ route('home') }}" class="nav-link">Beranda</a> <div class="mega-menu">
+    <div class="mega-container">
 
-      <div class="mega-menu">
-        <div class="mega-container">
-
-          <div class="mega-col">
-            <h4>Tentang Perusahaan</h4>
-            <ul>
-              <li><a href="#">Riwayat Singkat</a></li>
-              <li><a href="#">Budaya Perusahaan</a></li>
-              <li><a href="#">Profil Bisnis</a></li>
-              <li><a href="#">Pengembangan Bisnis</a></li>
-            </ul>
-          </div>
+      <div class="mega-col">
+        <h4>Tentang Perusahaan</h4>
+        <ul>
+          <li><a href="#">Riwayat Singkat</a></li>
+          <li><a href="#">Budaya Perusahaan</a></li>
+          <li><a href="#">Profil Bisnis</a></li>
+          <li><a href="#">Pengembangan Bisnis</a></li>
+        </ul>
       </div>
-    </li>
 
+      <div class="mega-col">
+        <h4>Layanan Kami</h4>
+        <ul>
+          <li><a href="#">Konsultasi</a></li>
+          <li><a href="#">Manajemen Aset</a></li>
+          <li><a href="#">Karir</a></li>
+        </ul>
+      </div>
+
+    </div>
+  </div>
+</li>
     <!-- MENU LAIN -->
     <li><a href="{{ route('about') }}">Profil Perusahaan</a></li>
     <li><a href="{{ route('products') }}">Produk</a></li>
